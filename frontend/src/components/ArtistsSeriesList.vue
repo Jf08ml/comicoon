fc6303689ae4dffa0caa6972756238c38f60f36c
Heriebrto Sanchez <hersac1@gmail.com>
2023-09-27 12:37:10 -0500

    Se integra vista series por artista y se componetiza

---
+++ frontend/src/components/ArtistsSeriesList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="section-artists">
+      <div class="container-list-artists">
+        <h3>These are the most popular artists</h3>
+        <div v-for="(artist, index) in artists" :key="index" class="list-artists">
+          <button class="btn-artist">{{ index + 1 }}. {{ artist.name }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const props = defineProps({
+  params: Object
+})
+
+const artists = ref({});
+
+onMounted(()=>{
+  console.log(props.params)
+  artists.value = props.params
+})
+
+</script>
+
+<style scoped></style>
