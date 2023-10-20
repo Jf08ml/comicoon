e7491cbfc5590d1fcf701d303884777c0ec88b32
Heri <hersac1@gmail.com>
2023-10-20 14:05:45 -0500

    Se implementa cambios en la estructura del NavbarMobile, se agregan efectos

--- frontend/src/layouts/Navbar/partials/NavbarMobile.vue
+++ frontend/src/layouts/Navbar/partials/NavbarMobile.vue
@@ -3,7 +3,7 @@
     <div class="nav-link-usersession" v-if="authStore.isAuthenticated">
       <UserSession />
     </div>
-    <div class="hamburger-icon" @click="toggleMenu">
+    <div class="hamburger-icon" @click="toggleMenu" >
       <v-icon
         :class="{ 'menu-icon': !isOpen }"
         v-if="!isOpen"
@@ -24,154 +24,36 @@
 
     <div class="menu" :style="{ display: isOpen ? 'block' : 'none' }">
       <div class="menu-open">
-
-        <router-link custom to="/realcomics">
-          <template v-slot="{ navigate }">
-            <button
-              @click="
-                () => {
-                  navigate();
-                  isOpen = false;
-                }
-              "
-              class="nav-link-mobile"
-            >
-            <div style="text-align: right">
-              <v-icon
-                name="fa-person-booth"
-                scale="1.2"
-                title="Real Comics"
-                color="#da2644"
-              />
-            </div>
-
-              <div style="text-align: left">
-                Real
-              </div>
-            </button>
-          </template>
-        </router-link>
-
-        <router-link custom to="/animatedcomics">
-          <template v-slot="{ navigate }">
-            <button
-              @click="
-                () => {
-                  navigate();
-                  isOpen = false;
-                }
-              "
-              class="nav-link-mobile"
-            >
-              <v-icon
-                name="si-gitbook"
-                scale="1.5"
-                title="Real Comics"
-                color="#da2644"
-              />
-              Animated
-            </button>
-          </template>
-        </router-link>
-        <router-link custom to="/earnmoney">
-          <template v-slot="{ navigate }">
-            <button
-              @click="
-                () => {
-                  navigate();
-                  isOpen = false;
-                }
-              "
-              class="nav-link-mobile"
-            >
-              <v-icon
-                name="gi-take-my-money"
-                scale="1.5"
-                title="Earn money"
-                color="#da2644"
-              />
-              Earn money
-            </button>
-          </template>
-        </router-link>
-        <router-link custom to="/login" v-if="!authStore.token">
-          <template v-slot="{ navigate }">
-            <button
-              @click="
-                () => {
-                  navigate();
-                  isOpen = false;
-                }
-              "
-              class="nav-link-mobile"
-            >
-              <v-icon
-                name="md-login"
-                scale="1.2"
-                title="Real Comics"
-                color="#da2644"
-              />
-              Log In
-            </button>
-          </template>
-        </router-link>
-        <router-link custom to="/signup" v-if="!authStore.token">
-          <template v-slot="{ navigate }">
-            <button
-              @click="
-                () => {
-                  navigate();
-                  isOpen = false;
-                }
-              "
-              class="nav-link-mobile"
-            >
-              <v-icon
-                name="md-newlabel-twotone"
-                scale="1.2"
-                title="Real Comics"
-                color="#da2644"
-              />
-              Sign Up
-            </button>
-          </template>
-        </router-link>
-
         <div v-for="(e, index) in menuObjects" :key="index">
-
-        <router-link custom :to="e.routeButton">
-          <template v-slot="{ navigate }">
-            <button
-              @click="
-                () => {
-                  navigate();
-                  isOpen = false;
-                }
-              "
-              class="nav-link-mobile"
-            >
-            <div style="text-align: right">
-              <v-icon
-                :name="e.iconName"
-                scale="1.2"
-                title="Real Comics"
-                color="#da2644"
-              />
-            </div>
-
-              <div style="text-align: left">
-                {{ e.titleMenu }}
+          <router-link custom :to="e.routeButton">
+            <template v-slot="{ navigate }">
+              <button
+                @click="
+                  () => {
+                    navigate();
+                    isOpen = false;
+                  }
+                "
+                class="nav-link-mobile"
+              >
+              <div class="iconMobileNavBar">
+                <v-icon
+                  :name="e.iconName"
+                  scale="1.2"
+                  title="Real Comics"
+                  color="#da2644"
+                />
               </div>
-            </button>
-          </template>
-        </router-link>
 
+                <div class="titleMobileNavBar">
+                  {{ e.titleMenu }}
+                </div>
+              </button>
+            </template>
+          </router-link>
         </div>
-
       </div>
     </div>
-
-
 
   </div>
 </template>
@@ -180,38 +62,37 @@
 import { ref } from "vue";
 import { useAuthStore } from "@/store/auth";
 import UserSession from "./UserSession.vue";
+import menuObjectService from "../../../services/menuService";
 
 const authStore = useAuthStore();
 const isOpen = ref(false);
+const menuObjects = ref([]);
 
 const toggleMenu = () => {
   isOpen.value = !isOpen.value;
 };
 
-
-const menuObjects = [{
-  titleMenu: "Real",
-  iconName: "fa-person-booth",
-  routeButton: "/realcomics",
-}];
-
-
+menuObjects.value = menuObjectService;
 
 </script>
 
 <style>
 .menu-icon {
-  transition: all 1s;
-  animation: fadeMenu 0.5s;
+  transition: all 0.5s;
+  animation: fadeMenu 0.5s ease-in-out;
 }
 
 @keyframes fadeMenu {
     0% {
-        transform: scale(0.1);
+        transform: scale(1);
+    }
+
+    25%{
+      transform: scale(0.1);
     }
 
     50%{
-      transform: scale(1.2);
+      transform: scale(1.5);
     }
 
     100%{
--- frontend/src/services/menuService.js
+++ frontend/src/services/menuService.js
@@ -1,6 +1,11 @@
 import MenuObjectModel from "./menuObjectModel";
 
+const menuObjectService = [
+	new MenuObjectModel("Real", "fa-person-booth", "/realcomics"),
+	new MenuObjectModel("Animated", "si-gitbook", "/animatedcomics"),
+	new MenuObjectModel("Earn Money", "gi-take-my-money", "/earnmoney"),
+	new MenuObjectModel("Login", "md-login", "/login"),
+	new MenuObjectModel("Sing up", "md-newlabel-twotone", "/signup")
+]
 
-const menuObjectService = new MenuObjectModel("Real", "fa-person-booth", "/realcomics");
-
-
+export default menuObjectService;
